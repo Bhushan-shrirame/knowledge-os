4c0771c88877745ebdc895074ca06272ba517232
Bhushan Shrirame <bhushan@uelement.in>
2026-02-16 16:27:42 +0530

    change name to merlin os

--- meta-kos-core/recipes-core/custom-boot/files/loading-anim.sh
+++ meta-kos-core/recipes-core/custom-boot/files/loading-anim.sh
@@ -8,21 +8,53 @@ clear > $CONSOLE
 # dmesg -n 1
 
 # Print your ASCII logo (Insert your ASCII art here)
-cat << 'EOF' > /etc/issue
- /$$   /$$                                   /$$                 /$$                      /$$$$$$   /$$$$$$ 
-| $$  /$$/                                  | $$                | $$                     /$$__  $$ /$$__  $$
-| $$ /$$/  /$$$$$$$   /$$$$$$  /$$  /$$  /$$| $$  /$$$$$$   /$$$$$$$  /$$$$$$   /$$$$$$ | $$  \ $$| $$  \__/
-| $$$$$/  | $$__  $$ /$$__  $$| $$ | $$ | $$| $$ /$$__  $$ /$$__  $$ /$$__  $$ /$$__  $$| $$  | $$|  $$$$$$ 
-| $$  $$  | $$  \ $$| $$  \ $$| $$ | $$ | $$| $$| $$$$$$$$| $$  | $$| $$  \ $$| $$$$$$$$| $$  | $$ \____  $$
-| $$\  $$ | $$  | $$| $$  | $$| $$ | $$ | $$| $$| $$_____/| $$  | $$| $$  | $$| $$_____/| $$  | $$ /$$  \ $$
-| $$ \  $$| $$  | $$|  $$$$$$/|  $$$$$/$$$$/| $$|  $$$$$$$|  $$$$$$$|  $$$$$$$|  $$$$$$$|  $$$$$$/|  $$$$$$/
-|__/  \__/|__/  |__/ \______/  \_____/\___/ |__/ \_______/ \_______/ \____  $$ \_______/ \______/  \______/ 
-                                                                     /$$  \ $$                              
-                                                                    |  $$$$$$/                              
-                                                                     \______/                               
-KnowledgeOS 1.0 AI  - \n \l
+# cat << "EOF" > /etc/issue                                                                                                                         
+                                                                                                                                             
+# MMMMMMMM               MMMMMMMM                                       lllllll   iiii                        OOOOOOOOO        SSSSSSSSSSSSSSS 
+# M:::::::M             M:::::::M                                       l:::::l  i::::i                     OO:::::::::OO    SS:::::::::::::::S
+# M::::::::M           M::::::::M                                       l:::::l   iiii                    OO:::::::::::::OO S:::::SSSSSS::::::S
+# M:::::::::M         M:::::::::M                                       l:::::l                          O:::::::OOO:::::::OS:::::S     SSSSSSS
+# M::::::::::M       M::::::::::M    eeeeeeeeeeee    rrrrr   rrrrrrrrr   l::::l iiiiiiinnnn  nnnnnnnn    O::::::O   O::::::OS:::::S            
+# M:::::::::::M     M:::::::::::M  ee::::::::::::ee  r::::rrr:::::::::r  l::::l i:::::in:::nn::::::::nn  O:::::O     O:::::OS:::::S            
+# M:::::::M::::M   M::::M:::::::M e::::::eeeee:::::eer:::::::::::::::::r l::::l  i::::in::::::::::::::nn O:::::O     O:::::O S::::SSSS         
+# M::::::M M::::M M::::M M::::::Me::::::e     e:::::err::::::rrrrr::::::rl::::l  i::::inn:::::::::::::::nO:::::O     O:::::O  SS::::::SSSSS    
+# M::::::M  M::::M::::M  M::::::Me:::::::eeeee::::::e r:::::r     r:::::rl::::l  i::::i  n:::::nnnn:::::nO:::::O     O:::::O    SSS::::::::SS  
+# M::::::M   M:::::::M   M::::::Me:::::::::::::::::e  r:::::r     rrrrrrrl::::l  i::::i  n::::n    n::::nO:::::O     O:::::O       SSSSSS::::S 
+# M::::::M    M:::::M    M::::::Me::::::eeeeeeeeeee   r:::::r            l::::l  i::::i  n::::n    n::::nO:::::O     O:::::O            S:::::S
+# M::::::M     MMMMM     M::::::Me:::::::e            r:::::r            l::::l  i::::i  n::::n    n::::nO::::::O   O::::::O            S:::::S
+# M::::::M               M::::::Me::::::::e           r:::::r           l::::::li::::::i n::::n    n::::nO:::::::OOO:::::::OSSSSSSS     S:::::S
+# M::::::M               M::::::M e::::::::eeeeeeee   r:::::r           l::::::li::::::i n::::n    n::::n OO:::::::::::::OO S::::::SSSSSS:::::S
+# M::::::M               M::::::M  ee:::::::::::::e   r:::::r           l::::::li::::::i n::::n    n::::n   OO:::::::::OO   S:::::::::::::::SS 
+# MMMMMMMM               MMMMMMMM    eeeeeeeeeeeeee   rrrrrrr           lllllllliiiiiiii nnnnnn    nnnnnn     OOOOOOOOO      SSSSSSSSSSSSSSS                                                                                                                                   
+                                                                       
+# UElement Softwares / MerlinOS v1.0
+# EOF
 
+cat << "EOF" > /etc/issue                                                                                                                         
+                                                                                                                                                                                                                   
+MMMMMMMM               MMMMMMMM                                       lllllll   iiii                   LLLLLLLLLLL               iiii          tttt                                   OOOOOOOOO        SSSSSSSSSSSSSSS 
+M:::::::M             M:::::::M                                       l:::::l  i::::i                  L:::::::::L              i::::i      ttt:::t                                 OO:::::::::OO    SS:::::::::::::::S
+M::::::::M           M::::::::M                                       l:::::l   iiii                   L:::::::::L               iiii       t:::::t                               OO:::::::::::::OO S:::::SSSSSS::::::S
+M:::::::::M         M:::::::::M                                       l:::::l                          LL:::::::LL                          t:::::t                              O:::::::OOO:::::::OS:::::S     SSSSSSS
+M::::::::::M       M::::::::::M    eeeeeeeeeeee    rrrrr   rrrrrrrrr   l::::l iiiiiiinnnn  nnnnnnnn      L:::::L               iiiiiiittttttt:::::ttttttt        eeeeeeeeeeee    O::::::O   O::::::OS:::::S            
+M:::::::::::M     M:::::::::::M  ee::::::::::::ee  r::::rrr:::::::::r  l::::l i:::::in:::nn::::::::nn    L:::::L               i:::::it:::::::::::::::::t      ee::::::::::::ee  O:::::O     O:::::OS:::::S            
+M:::::::M::::M   M::::M:::::::M e::::::eeeee:::::eer:::::::::::::::::r l::::l  i::::in::::::::::::::nn   L:::::L                i::::it:::::::::::::::::t     e::::::eeeee:::::eeO:::::O     O:::::O S::::SSSS         
+M::::::M M::::M M::::M M::::::Me::::::e     e:::::err::::::rrrrr::::::rl::::l  i::::inn:::::::::::::::n  L:::::L                i::::itttttt:::::::tttttt    e::::::e     e:::::eO:::::O     O:::::O  SS::::::SSSSS    
+M::::::M  M::::M::::M  M::::::Me:::::::eeeee::::::e r:::::r     r:::::rl::::l  i::::i  n:::::nnnn:::::n  L:::::L                i::::i      t:::::t          e:::::::eeeee::::::eO:::::O     O:::::O    SSS::::::::SS  
+M::::::M   M:::::::M   M::::::Me:::::::::::::::::e  r:::::r     rrrrrrrl::::l  i::::i  n::::n    n::::n  L:::::L                i::::i      t:::::t          e:::::::::::::::::e O:::::O     O:::::O       SSSSSS::::S 
+M::::::M    M:::::M    M::::::Me::::::eeeeeeeeeee   r:::::r            l::::l  i::::i  n::::n    n::::n  L:::::L                i::::i      t:::::t          e::::::eeeeeeeeeee  O:::::O     O:::::O            S:::::S
+M::::::M     MMMMM     M::::::Me:::::::e            r:::::r            l::::l  i::::i  n::::n    n::::n  L:::::L         LLLLLL i::::i      t:::::t    tttttte:::::::e           O::::::O   O::::::O            S:::::S
+M::::::M               M::::::Me::::::::e           r:::::r           l::::::li::::::i n::::n    n::::nLL:::::::LLLLLLLLL:::::Li::::::i     t::::::tttt:::::te::::::::e          O:::::::OOO:::::::OSSSSSSS     S:::::S
+M::::::M               M::::::M e::::::::eeeeeeee   r:::::r           l::::::li::::::i n::::n    n::::nL::::::::::::::::::::::Li::::::i     tt::::::::::::::t e::::::::eeeeeeee   OO:::::::::::::OO S::::::SSSSSS:::::S
+M::::::M               M::::::M  ee:::::::::::::e   r:::::r           l::::::li::::::i n::::n    n::::nL::::::::::::::::::::::Li::::::i       tt:::::::::::tt  ee:::::::::::::e     OO:::::::::OO   S:::::::::::::::SS 
+MMMMMMMM               MMMMMMMM    eeeeeeeeeeeeee   rrrrrrr           lllllllliiiiiiii nnnnnn    nnnnnnLLLLLLLLLLLLLLLLLLLLLLLLiiiiiiii         ttttttttttt      eeeeeeeeeeeeee       OOOOOOOOO      SSSSSSSSSSSSSSS   
+                                                               
+UElement Softwares / MerlinLiteOS v1.0
 EOF
+
+
+# 3. Silence kernel noise to prevent it from overwriting the logo
+dmesg -n 1
 
 # 4. Redirect all FUTURE boot noise to nothingness
 exec >/dev/null 2>&1
